c42fa95a0b6159124497e9ab989801ce77b659b1
Ravi Shankar <ravishankerdas020@gmail.com>
2025-06-20 23:28:34 +0530

    added db stuff

--- backend/src/api/db.py
+++ backend/src/api/db.py
@@ -5,13 +5,10 @@ from sqlmodel import Session, SQLModel
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
-if DATABASE_URL == "":
+if not DATABASE_URL:
     raise NotImplementedError("`DATABASE_URL` needs to be set.")
 
-try:
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgres+psycopg://") # type: ignore
-except Exception as e:
-    raise ValueError(f"Invalid DATABASE_URL format: {DATABASE_URL}") from e
+DATABASE_URL = DATABASE_URL.replace("postgres://", "postgres+psycopg://")
 
 engine = sqlmodel.create_engine(DATABASE_URL)
 
